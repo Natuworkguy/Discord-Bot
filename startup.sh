204f235e3c417c0b5f37b5b9bdba6f22fac5d275
Nathan C. <149914029+Natuworkguy@users.noreply.github.com>
2025-05-19 18:07:01 -0400

    Update startup.sh
    Added compatibility and error handling. The script must be run as root.

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
-python3 ~/dbot/banner.py
-sudo apt install -y screen python3 python3-pip
-pip install -r ~/dbot/requirements.txt
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be ran as root."
+    exit 1
+fi
+apt install -y screen python3 python3-pip || exit 1
+pip install -r ./requirements.txt || exit 1
+python3 ./banner.py || exit 1
 echo "Completed dependency install"
-screen -DdmS discord python3 ~/dbot/main.py
+screen -DdmS discord python3 .dbot/main.py || exit 1
 echo "Proccess started successfully"
